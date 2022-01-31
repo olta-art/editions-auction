eb8b26023af55f832ba6d5adfee16b10fd919195
george-e-d-g-e <george@e-d-g-e.co.uk>
2022-01-31 14:53:23 +0000

    reorder and add docs to external functions

--- contracts/EditionsAuction.sol
+++ contracts/EditionsAuction.sol
@@ -14,6 +14,9 @@ import {Counters} from "@openzeppelin/contracts/utils/Counters.sol";
 import {IEditionSingleMintable} from "@zoralabs/nft-editions-contracts/contracts/IEditionSingleMintable.sol";
 import {IEditionsAuction} from "./IEditionsAuction.sol";
 
+/**
+ * @title An open dutch auction house, for initial drops of limited edition nft contracts.
+ */
 contract EditionsAuction is IEditionsAuction, ReentrancyGuard, PullPayment {
   using SafeMath for uint256;
   using Counters for Counters.Counter;
@@ -26,18 +29,37 @@ contract EditionsAuction is IEditionsAuction, ReentrancyGuard, PullPayment {
 
   Counters.Counter private _auctionIdTracker;
 
-   /**
-    * @notice Require that the specified auction exists
-    */
+  /**
+   * @notice Require that the specified auction exists
+   */
   modifier auctionExists(uint256 auctionId) {
-      require(_exists(auctionId), "Auction doesn't exist");
-      _;
+    require(_exists(auctionId), "Auction doesn't exist");
+    _;
   }
 
+
+  /**
+   * Constructor
+   */
   constructor() {
     minStepTime = 2 * 60; // 2 minutes
   }
 
+  /**
+   * @notice Create an auction.
+   * @dev Store the auction details in the auctions mapping and emit an AuctionCreated event.
+   * If there is no curator, or if the curator is the auction creator,
+   * automatically approve the auction and emit an AuctionApproved event.
+   * @param editionContract the contract of which NFT's will be minted
+   * @param startTimestamp the time the auction will start
+   * @param duration the duration the auction will run for
+   * @param startPrice the price in eth the auction will start at
+   * @param endPrice the price in eth the auction will end at
+   * @param numberOfPriceDrops the number of times the price will drop between starting and ending price
+   * @param curator the address of the allocated curator
+   * @param curatorRoyaltyBPS the royalty the curator will recieve per purchase in basis points
+   * @return auction id
+   */
   function createAuction(
     address editionContract,
     uint256 startTimestamp,
@@ -48,7 +70,6 @@ contract EditionsAuction is IEditionsAuction, ReentrancyGuard, PullPayment {
     address curator,
     uint256 curatorRoyaltyBPS
   ) external override nonReentrant returns (uint256) {
-    
     // TODO: find or get EditionSingleMintable interfaceId so we can check the contract is a match
     // require(IEditionSingleMintable(editionContract).supportsInterface(editionSingleMintableinterfaceId)
     // artist
@@ -103,14 +124,12 @@ contract EditionsAuction is IEditionsAuction, ReentrancyGuard, PullPayment {
     return auctionId;
   }
 
-  // Allows curator to approve auctions first
-  function setAuctionApproval(uint256 auctionId, bool approved) external override auctionExists(auctionId) {
-    require(msg.sender == auctions[auctionId].curator, "must be curator");
-    require(block.timestamp < auctions[auctionId].startTimestamp, "Auction has already started");
-    // TODO: see if auction should be cancled/ended if approval is set to false?
-    _approveAuction(auctionId, approved);
-  }
-
+  /**
+   * @notice Purchases an NFT
+   * @dev mints an NFT and splits purchase fee between creator and curator
+   * @param auctionId the id of the auction
+   * @return the id of the NFT
+   */
   function purchase(uint256 auctionId) external payable override auctionExists(auctionId) returns (uint256){
     require(auctions[auctionId].approved, "Auction has not been approved");
     require(block.timestamp >= auctions[auctionId].startTimestamp, "Auction has not started yet");
@@ -150,6 +169,48 @@ contract EditionsAuction is IEditionsAuction, ReentrancyGuard, PullPayment {
     return _numberCanMint(auctionId);
   }
 
+  /**
+   * @notice allows curator to approve auction
+   * @dev sets auction approved to approval and emits an AuctionApprovalUpdated event
+   * @param auctionId the id of the auction
+   * @param approved the curators approval decision
+   */
+  function setAuctionApproval(uint256 auctionId, bool approved) external override auctionExists(auctionId) {
+    require(msg.sender == auctions[auctionId].curator, "must be curator");
+    require(block.timestamp < auctions[auctionId].startTimestamp, "Auction has already started");
+    // TODO: see if auction should be cancled/ended if approval is set to false?
+    _approveAuction(auctionId, approved);
+  }
+
+  /**
+   * @notice gets the current sale price of an auction
+   * @dev calculates the price based on the block.timestamp
+   * @param auctionId the id of the auction
+   * @return price in wei
+   */
+  function getSalePrice(uint256 auctionId) external view override returns (uint256) {
+    return _getSalePrice(auctionId);
+  }
+
+  /**
+   * @dev Returns the payments owed to an address.
+   * @param dest The creditor's address.
+   */
+  function paymentsOwed(address dest) external view virtual override returns (uint256) {
+      return payments(dest);
+  }
+
+  // TODO: check if need reentrency guard here: https://docs.openzeppelin.com/contracts/4.x/api/security#PullPayment-withdrawPayments-address-payable-
+  /**
+   * @dev withdraws the payments owed to an address.
+   * @param paye the address to pay
+   */
+  function withdraw(address payable paye) external override {
+    require(payments(paye) != 0,"account is not owed any payments");
+
+    withdrawPayments(paye);
+  }
+
   function _numberCanMint(uint256 auctionId) internal view returns (uint256) {
     return IEditionSingleMintable(auctions[auctionId].editionContract).numberCanMint();
   }
@@ -158,25 +219,18 @@ contract EditionsAuction is IEditionsAuction, ReentrancyGuard, PullPayment {
     return auctions[auctionId].creator != address(0);
   }
 
-  function getSalePrice(uint256 auctionId) external view override returns (uint256) {
-    return _getSalePrice(auctionId);
-  }
-
   function _approveAuction(uint256 auctionId, bool approved) internal {
     auctions[auctionId].approved = approved;
     emit AuctionApprovalUpdated(auctionId, auctions[auctionId].editionContract, approved);
   }
 
-  /**
-  * @dev returns sale price based on block.timestamp
-  */
   function _getSalePrice(uint256 auctionId) internal view returns (uint256) {
-    // return ending price if auction is over
+    // return endPrice if auction is over
     if(block.timestamp > auctions[auctionId].startTimestamp.add(auctions[auctionId].duration)){
       return auctions[auctionId].endPrice;
     }
 
-    // return starting price if auction hasn't dropped yet
+    // return startPrice if auction hasn't started yet
     if(block.timestamp < auctions[auctionId].startTimestamp.add(auctions[auctionId].stepTime)){
       return auctions[auctionId].startPrice;
     }
@@ -189,21 +243,5 @@ contract EditionsAuction is IEditionsAuction, ReentrancyGuard, PullPayment {
     // transalte -1 so endPrice is after auction.duration
     return auctions[auctionId].startPrice.sub(auctions[auctionId].stepPrice.mul(dropNum - 1));
   }
-
-  /**
-  * @dev Returns the payments owed to an address.
-  * @param dest The creditor's address.
-  */
-  function paymentsOwed(address dest) external view virtual override returns (uint256) {
-      return payments(dest);
-  }
-
-  // TODO: check if need reentrency guard here: https://docs.openzeppelin.com/contracts/4.x/api/security#PullPayment-withdrawPayments-address-payable-
-  function withdraw(address payable paye) external override {
-    require(payments(paye) != 0,"account is not owed any payments");
-
-    withdrawPayments(paye);
-  }
-
   // TODO: endAuction end everything if sold out remove form auctions mapping?
 }
