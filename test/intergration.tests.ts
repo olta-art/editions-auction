cf8cdeb847758b59f2f3628e91f2c71f432d0779
george-e-d-g-e <george@e-d-g-e.co.uk>
2022-02-09 14:27:05 +0000

    test: add fuzzy WETH auctions

--- test/intergration.tests.ts
+++ test/intergration.tests.ts
@@ -1,6 +1,6 @@
 import { expect } from "chai";
 import "@nomiclabs/hardhat-ethers";
-import { ethers, deployments } from "hardhat";
+import { ethers, deployments, network } from "hardhat";
 
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import {
@@ -16,13 +16,20 @@ import {
   mineToTimestamp,
   getEventArguments,
   deployWETH,
+  getRandomInt,
+  getPreviousBlockTimestamp,
+  equalWithin
 } from "./utils"
 
 
-describe.only("EditionsAuction", () => {
+describe("EditionsAuction", () => {
   let curator: SignerWithAddress;
   let creator: SignerWithAddress;
   let collector: SignerWithAddress;
+  let curatorAddress: string;
+  let creatorAddress: string;
+  let collectorAddress: string;
+
   let SingleEditonCreator: SingleEditionMintableCreator;
   let EditionsAuction: EditionsAuction;
   let SingleEdition: SingleEditionMintable;
@@ -78,6 +85,96 @@ describe.only("EditionsAuction", () => {
     )
   }
 
+  const genarateAuctionParams = async (hasCurator: boolean = false) => {
+
+    // start time
+    const startTime = Math.floor((Date.now() / 1000)) + (60 * getRandomInt(2, 100))
+
+    // duration
+    const mins = 60 * getRandomInt(2, 59)
+    const hours = 60 * 60 * getRandomInt(0, 100)
+    const duration = mins + hours
+
+    // prices
+    const random3DecFloat = () => Math.floor(Math.random() * 1000) / 1000 // 0.000-1.000
+    const startPrice = getRandomInt(1, 3) + random3DecFloat()
+    const endPrice = getRandomInt(0, startPrice - 1) + random3DecFloat()
+
+    // drops
+    const twoMin = 60 * 2
+    const maxDrops = Math.min(duration / twoMin, 255) // 2mins
+    const numberOfPriceDrops = getRandomInt(1, maxDrops)
+
+    // curator
+    const curatorAddress = hasCurator ? await curator.getAddress() : ethers.constants.AddressZero
+    const curatorRoyaltyBPS = hasCurator ? getRandomInt(0, 100) * 100 : 0
+
+    return {
+      startTime,
+      duration,
+      startPrice: ethers.utils.parseEther(`${startPrice}`),
+      endPrice: ethers.utils.parseEther(`${endPrice}`),
+      numberOfPriceDrops,
+      curator: curatorAddress,
+      curatorRoyaltyBPS
+    }
+  }
+
+  const generatePurchases = async (auctionId: number) => {
+    // get auction
+    const auction = await EditionsAuction.auctions(auctionId)
+
+    const numberCanMint = await EditionsAuction.numberCanMint(auctionId)
+    const maxTimeIncrease = auction.duration.div(numberCanMint).toNumber() * 3
+
+    const purchaseEdition = async () => {
+      const previousBlockTimestamp = await getPreviousBlockTimestamp()
+
+      // increase block timestamp
+      const timeIncrease = getRandomInt(0, maxTimeIncrease)
+      await mineToTimestamp(ethers.BigNumber.from(previousBlockTimestamp + timeIncrease))
+
+      // make purchase
+      const salePrice = await EditionsAuction.getSalePrice(auctionId)
+      return await EditionsAuction.connect(collector).purchase(auctionId, salePrice)
+    }
+
+    // generator function
+    const purchases = async function * () {
+      //editions left
+      let leftToMint = numberCanMint.toNumber()
+      while(leftToMint > 0){
+        leftToMint--
+        yield await purchaseEdition()
+      }
+    }
+
+    return purchases
+  }
+
+  const runAuction = async (auctionId: number) => {
+    // get auction
+    const auction = await EditionsAuction.auctions(auctionId)
+    // goto start of auction
+    await mineToTimestamp(auction.startTimestamp)
+
+    const purchases = await generatePurchases(auctionId)
+
+    let total = ethers.BigNumber.from(0)
+    // purchase all editions
+    for await ( const purchase of purchases() ){
+      expect(purchase).to.emit(EditionsAuction, "EditionPurchased")
+    }
+
+    // purchase when no editons left
+    const salePrice = await EditionsAuction.getSalePrice(0)
+    await expect(
+      EditionsAuction.connect(collector).purchase(0, salePrice)
+    ).to.be.revertedWith("Sold out")
+
+    return total
+  }
+
   beforeEach(async () => {
     const { SingleEditionMintableCreator, EditionsAuction: EditionsAuctionContract } = await deployments.fixture([
       "SingleEditionMintableCreator",
@@ -100,8 +197,152 @@ describe.only("EditionsAuction", () => {
     creator = _creator;
     collector = _collector;
 
+    curatorAddress = await curator.getAddress();
+    creatorAddress = await creator.getAddress();
+    collectorAddress = await collector.getAddress();
+
     SingleEdition = await createEdition()
     weth = await deployWETH()
+  })
+
+  //TODO: WBTC auction with no curator -> (different decimal places)
+  //TODO: WBTC auction with curator -> (different decimal places)
+  //TODO: NFT that is not zora NFT edition?
+  //TODO: stress test multiple auctions at once?
+
+  describe.only("WETH auction with curator [FUZZY]", async () => {
+    let auction: any
+
+    beforeEach(async () => {
+      weth = await deployWETH()
+      // deposit WETH - enough for 100 editons at max price
+      await weth.connect(collector).deposit({ value: ethers.utils.parseEther("40.0") });
+      await weth.connect(collector).approve(EditionsAuction.address, ethers.utils.parseEther("40.0"))
+
+      const options =  await genarateAuctionParams(true)
+      await createAuction(creator, options)
+
+      auction = await EditionsAuction.auctions(0)
+      await SingleEdition.connect(creator)
+        .setApprovedMinter(EditionsAuction.address, true)
+
+      if(!auction.approved){
+        await EditionsAuction.connect(curator).setAuctionApproval(0, true)
+      }
+    })
+    it("should mint tokens", async () => {
+      await runAuction(0)
+
+      const collectorNFTBalance = await SingleEdition.balanceOf(collectorAddress)
+      const editionSize = ethers.BigNumber.from(10)
+      expect(
+        collectorNFTBalance.eq(editionSize)
+      ).to.eq(true)
+    })
+    it("should pay creator", async () => {
+
+      const collectorBalanceStart = await weth.balanceOf(collectorAddress)
+      await runAuction(0)
+      const collectorBalanceEnd = await weth.balanceOf(collectorAddress)
+
+      const total = collectorBalanceStart.sub(collectorBalanceEnd)
+
+      // NOTE: ethers BigNumber seems to be rounding differently to solidity
+      // but should be equal to within 5 wei
+      const five_wei = ethers.BigNumber.from(5)
+
+      const curatorBalanceExpected = (total.mul(auction.curatorRoyaltyBPS)).div(10000)
+      const creatorBalanceExpected = total.sub(curatorBalanceExpected)
+
+      const creatorBalanceActual = await weth.balanceOf(creatorAddress)
+
+      expect(
+        equalWithin(creatorBalanceActual, creatorBalanceExpected, five_wei)
+      ).to.eq(true)
+
+      // zero balance locked in contract
+      const contractBalance = await weth.balanceOf(EditionsAuction.address)
+      expect(contractBalance).to.eq(ethers.utils.parseEther("0.0"))
+    })
+
+    it("should pay curator", async () => {
+      const collectorBalanceStart = await weth.balanceOf(collectorAddress)
+      await runAuction(0)
+      const collectorBalanceEnd = await weth.balanceOf(collectorAddress)
+
+      const total = collectorBalanceStart.sub(collectorBalanceEnd)
+
+      // NOTE: ethers BigNumber seems to be rounding differently to solidity
+      // but should be equal to within 5 wei
+      const five_wei = ethers.BigNumber.from(5)
+
+      const curatorBalanceActual = await weth.balanceOf(curatorAddress)
+      const curatorBalanceExpected = (total.mul(auction.curatorRoyaltyBPS)).div(10000)
+      expect(
+        equalWithin(curatorBalanceActual, curatorBalanceExpected, five_wei)
+      ).to.eq(true)
+    })
+
+    it("should not lock any funds in contract", async () => {
+      await runAuction(0)
+
+      // zero balance locked in contract
+      const contractBalance = await weth.balanceOf(EditionsAuction.address)
+      expect(contractBalance).to.eq(ethers.utils.parseEther("0.0"))
+    })
+  })
+
+  describe.only("WETH auction with no curator [FUZZY]", async () => {
+    let auction: any
+
+    beforeEach(async () => {
+      weth = await deployWETH()
+      // deposit WETH - enough for 100 editons at max price
+      await weth.connect(collector).deposit({ value: ethers.utils.parseEther("40.0") });
+      await weth.connect(collector).approve(EditionsAuction.address, ethers.utils.parseEther("40.0"))
+
+      const options =  await genarateAuctionParams(false) // no curator
+      await createAuction(creator, options)
+
+      auction = await EditionsAuction.auctions(0)
+      await SingleEdition.connect(creator)
+        .setApprovedMinter(EditionsAuction.address, true)
+
+      if(!auction.approved){
+        await EditionsAuction.connect(curator).setAuctionApproval(0, true)
+      }
+    })
+    it("should mint tokens", async () => {
+      await runAuction(0)
+
+      const collectorNFTBalance = await SingleEdition.balanceOf(collectorAddress)
+      const editionSize = ethers.BigNumber.from(10)
+      expect(
+        collectorNFTBalance.eq(editionSize)
+      ).to.eq(true)
+    })
+    it("should pay creator", async () => {
+
+      const collectorBalanceStart = await weth.balanceOf(collectorAddress)
+      await runAuction(0)
+      const collectorBalanceEnd = await weth.balanceOf(collectorAddress)
+
+      const total = collectorBalanceStart.sub(collectorBalanceEnd)
+
+      const creatorBalanceActual = await weth.balanceOf(creatorAddress)
+
+      expect(
+        total.eq(creatorBalanceActual)
+      ).to.eq(true)
+    })
+
+    it("should not lock any funds in contract", async () => {
+      await runAuction(0)
+
+      // zero balance locked in contract
+      const contractBalance = await weth.balanceOf(EditionsAuction.address)
+      expect(contractBalance).to.eq(ethers.utils.parseEther("0.0"))
+    })
   })
 
 
@@ -174,8 +415,6 @@ describe.only("EditionsAuction", () => {
       // un-pause auto mine
       await setAutoMine(true)
     })
-
-    // TODO: curator auction stress tests
   })
 
 })
--- test/utils.ts
+++ test/utils.ts
@@ -22,3 +22,22 @@ export const deployWETH = async () => {
   const [deployer] = await ethers.getSigners();
   return (await (await ethers.getContractFactory("WETH")).deploy()) as WETH;
 };
+
+export const getRandomInt = (min: number, max: number) => {
+  min = Math.ceil(min)
+  max = Math.floor(max)
+  return Math.floor(Math.random() * (max - min) + min)
+}
+
+export const getPreviousBlockTimestamp = async () => {
+    const blockNumBefore = await ethers.provider.getBlockNumber();
+    const blockBefore = await ethers.provider.getBlock(blockNumBefore);
+    return blockBefore.timestamp;
+}
+
+// Helper function to compare two bignumbers within a margin of error
+// returns true if difference is less than or equal to errorMargin
+export const equalWithin = (bn1: BigNumber, bn2: BigNumber, errorMargin: BigNumber) => {
+  const difference = bn1.sub(bn2)
+  return difference.abs().lte(errorMargin)
+}
