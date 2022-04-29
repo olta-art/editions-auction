f1645bc2562043f278e9ba655de38a407653dcb9
george-e-d-g-e <george@e-d-g-e.co.uk>
2022-04-29 14:53:40 +0100

    feat: add helper contract and script to get Interface Id's

---
+++ scripts/helpers/getInterfaceId.ts
@@ -0,0 +1,18 @@
+// deploys GetInterfaceId helper contract
+// and logs the interfaceId's to in the console
+// run this script: yarn hardhat run scripts/helpers/getInterfaceId.ts
+
+import { Helpers } from "../../typechain";
+import { ethers } from "hardhat";
+
+const getInterfaceIds = async () => {
+  // deploy helpers
+  const helpersFactory = await ethers.getContractFactory("Helpers")
+  const helpers = await helpersFactory.deploy() as Helpers;
+
+  // log the interface Id's
+  await helpers.getEditionsInterfaceId()
+  await helpers.getSeededEditionsInterfaceId()
+}
+
+getInterfaceIds()
