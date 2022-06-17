d3e0d88d74ad6a444541a4c8a1c707a0e9ec5ad7
george-e-d-g-e <george@e-d-g-e.co.uk>
2022-06-17 11:57:24 +0100

    sol/Utils: refactor add absract utils contract

---
+++ contracts/Utils.sol
@@ -0,0 +1,152 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity 0.8.6;
+import {IEditionsAuction} from "./IEditionsAuction.sol";
+import {SafeMath} from "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+
+interface ERC721 {
+  function balanceOf(address owner) external view returns (uint256);
+}
+
+abstract contract Utils is IEditionsAuction {
+  using SafeMath for uint256;
+  using SafeERC20 for IERC20;
+
+  function _isCollector(address editionId, address collector) internal view returns (bool) {
+    return (ERC721(editionId).balanceOf(collector) > 0);
+  }
+
+  function _handlePurchasePayment(Auction memory auction, uint256 salePrice) internal{
+    IERC20 token = IERC20(auction.auctionCurrency);
+
+    // NOTE: msg.sender would need to approve this contract with currency before making a purchase
+    // If intergrating with zora v3 the market would hold the funds and handle royalties differently.
+    // through royalties finders, and protocal fees
+    // TODO: respect royalties on NFT contract (v3 intergration could solve this)
+
+    // NOTE: modified from v3 for now. A full intergration would be better if we go that route
+    // https://github.com/ourzora/v3/blob/main/contracts/common/IncomingTransferSupport/V1/IncomingTransferSupportV1.sol
+
+    // We must check the balance that was actually transferred to this contract,
+    // as some tokens impose a transfer fee and would not actually transfer the
+    // full amount to the market, resulting in potentally locked funds
+    uint256 beforeBalance = token.balanceOf(address(this));
+    token.safeTransferFrom(msg.sender, address(this), salePrice);
+    uint256 afterBalance = token.balanceOf(address(this));
+    require(beforeBalance + salePrice == afterBalance, "_handleIncomingTransfer token transfer call did not transfer expected amount");
+
+    // if no curator, add payment to creator
+    if(auction.curator == address(0)){
+      token.safeTransfer(
+        auction.creator,
+        salePrice
+      );
+    }
+
+    // else split payment between curator and creator
+    else {
+      uint256 curatorFee = (salePrice.mul(auction.curatorRoyaltyBPS)).div(10000);
+      token.safeTransfer(
+        auction.curator,
+        curatorFee
+      );
+
+      uint256 creatorFee = salePrice.sub(curatorFee);
+      token.safeTransfer(
+        auction.creator,
+        creatorFee
+      );
+    }
+
+    return;
+  }
+
+  function _getSalePrice(Auction memory auction) internal view returns (uint256) {
+    // return endPrice if auction is over
+    if(block.timestamp > auction.startTimestamp.add(auction.duration)){
+      return auction.endPrice;
+    }
+
+    uint256 stepTime = _calcStepTime(auction);
+
+    // return startPrice if auction hasn't started yet
+    if(block.timestamp <= auction.startTimestamp.add(stepTime)){
+      return auction.startPrice;
+    }
+
+    // calculate price based of block.timestamp
+    uint256 timeSinceStart = block.timestamp.sub(auction.startTimestamp);
+    uint256 dropNum = _floor(timeSinceStart, stepTime).div(stepTime);
+
+    uint256 stepPrice = _calcStepPrice(auction);
+
+    // transalte -1 so endPrice is after auction.duration
+    uint256 price = auction.startPrice.sub(stepPrice.mul(dropNum - 1));
+
+    return _floor(
+      price,
+      _unit10(stepPrice, 2)
+    );
+  }
+
+  function _calcStepPrice(
+    Auction memory auction
+  ) internal pure returns (uint256) {
+      return auction.startPrice.sub(auction.endPrice).div(auction.numberOfPriceDrops);
+  }
+
+  function _calcStepTime(
+    Auction memory auction
+  ) internal pure returns (uint256) {
+      return auction.duration.div(auction.numberOfPriceDrops);
+  }
+
+  /**
+   * @dev floors number to nearest specified unit
+   * @param value number to floor
+   * @param unit number specififying the smallest uint to floor to
+   * @return result number floored to nearest unit
+  */
+  function _floor(uint256 value, uint256 unit) internal pure returns (uint256){
+    uint256 remainder = value.mod(unit);
+    return value - remainder;
+  }
+
+  /** @dev calculates exponent from given value number of digits minus the offset
+   * and returns 10 to the power of the resulting exponent
+   * @param value the number of which the exponent is calculated from
+   * @param exponentOffset the number to offset the resulting exponent
+   * @return result 10 to the power of calculated exponent
+   */
+  function _unit10(uint256 value, uint256 exponentOffset) internal pure returns (uint256){
+    uint256 exponent = _getDigits(value);
+
+    if (exponent == 0) {
+        return 0;
+    }
+
+    if(exponent < exponentOffset || exponentOffset == 0){
+      exponentOffset = 1;
+    }
+
+    return 10**(exponent - exponentOffset);
+  }
+
+   /**
+    * @dev gets number of digits of a number
+    * @param value number to count digits of
+    * @return digits number of digits in value
+    */
+  function _getDigits(uint256 value) internal pure returns (uint256) {
+      if (value == 0) {
+          return 0;
+      }
+      uint256 digits;
+      while (value != 0) {
+          digits++;
+          value /= 10;
+      }
+      return digits;
+  }
+}
