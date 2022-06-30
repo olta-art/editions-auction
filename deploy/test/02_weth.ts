1fd1ca829e08a568650cc4f7bd3c348155e37b6a
george-e-d-g-e <george@e-d-g-e.co.uk>
2022-06-30 15:09:09 +0100

    skip test contract deployments if not hardhat network

--- deploy/test/02_weth.ts
+++ deploy/test/02_weth.ts
@@ -1,11 +1,14 @@
 module.exports = async ({ getNamedAccounts, deployments }: any) => {
-  const { deploy } = deployments;
-  const { deployer } = await getNamedAccounts();
+  const { deploy } = deployments
+  const { deployer } = await getNamedAccounts()
 
   await deploy("WETH", {
     from: deployer,
     args: [],
     log: true,
-  });
-};
-module.exports.tags = ["WETH"];
+  })
+}
+
+module.exports.tags = ["WETH"]
+
+module.exports.skip = async ({network}: any ) => network.name != "hardhat"
